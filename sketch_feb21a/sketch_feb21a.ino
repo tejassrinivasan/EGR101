f488ec8fb11085d403d1a5400c49b1104b1d0c7f
Tejas Srinivasan <tejas.srinivasan@duke.edu>
2020-03-04 13:11:36 -0500

    updated

--- sketch_feb21a/sketch_feb21a.ino
+++ sketch_feb21a/sketch_feb21a.ino
@@ -8,18 +8,14 @@
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
 */
-  
- 
+
 #include "ESP32_MailClient.h"
 #include <WiFi.h>
 #include "time.h"
 
-// REPLACE WITH YOUR NETWORK CREDENTIALS
 const char* ssid = "DukeVisitor";
 const char* password = "";
 
-// To send Email using Gmail use port 465 (SSL) and SMTP Server smtp.gmail.com
-// YOU MUST ENABLE less secure app option https://myaccount.google.com/lesssecureapps?pli=1
 #define emailSenderAccount    "suppliesdetect@gmail.com"
 #define emailSenderPassword   "missingtooth"
 #define emailRecipient        "suppliesdetect@gmail.com"
@@ -29,42 +25,35 @@ const char* password = "";
 
 const int suppliesButton = 13;
 const int suppliesLED = 23;
-const int tempButton = 26;
+const int tempHotButton = 26;
+const int tempColdButton = 32;
 const int tempLED = 5;
+bool disabled0;
+bool disabled1;
 
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = -18000;
 const int   daylightOffset_sec = 3600;
 
-
-// The Email Sending data object contains config and data to send
 SMTPData smtpData;
 
-// Callback function to get the Email sending status
 void sendCallback(SendStatus info);
 
 void sendEmail(int button, int LED, int msg);
 
-void printLocalTime()
-{
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
-
 void setup() {
   pinMode(suppliesButton, INPUT_PULLUP);
-  pinMode(tempButton, INPUT_PULLUP);
+  pinMode(tempHotButton, INPUT_PULLUP);
+  pinMode(tempColdButton, INPUT_PULLUP);
   pinMode(suppliesLED, OUTPUT);
   pinMode(tempLED, OUTPUT);
   digitalWrite(23, LOW);
   digitalWrite(5, LOW);
+  disabled0 = false;
+  disabled1 = false;
   Serial.begin(115200);
+  
   Serial.println();
-
   Serial.print("Connecting");
 
   WiFi.begin(ssid, password);
@@ -83,14 +72,49 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
-  printLocalTime();
-  
-  if (digitalRead(suppliesButton) == false) {
+  struct tm timeinfo;
+  getLocalTime(&timeinfo);
+  long timePushed;
+
+  if (digitalRead(suppliesButton) == false && !disabled0) {
+    //if it is not disabled send email and disable
     sendEmail(suppliesLED, 0);
+    disabled0 = true;
+    printLocalTime();
   }
-  else if (digitalRead(tempButton) == false) {
-    sendEmail(tempLED, 1);
+
+  //if it is 6 oclock and disabled enable it
+  if ((timeinfo.tm_hour == 06) && disabled0) {
+    disabled0 = false;
+    printLocalTime();
+  }
+
+  if (digitalRead(tempHotButton) == false) {
+    checkenable();
+    if (!disabled1) {
+      sendEmail(tempLED, 1);
+      disabled1 = true;
+      timePushed = millis();
+      printLocalTime();
+    }
+  }
+
+  if (digitalRead(tempColdButton) == false) {
+    checkenable();
+    if (!disabled1) {
+      sendEmail(tempLED, 2);
+      disabled1 = true;
+      timePushed = millis();
+      printLocalTime();
+    }
+  }
+}
+
+void checkenable() {
+  //if 30 min pass and disabled, enable it
+  if (((millis() > timePushed + 1800000) || millis() < timePushed) && disabled1) {
+    disabled1 = false;
+    printLocalTime();
   }
 }
 
@@ -100,10 +124,6 @@ void sendEmail(int LED, int msg) {
 
   // Set the SMTP Server Email host, port, account and password
   smtpData.setLogin(smtpServer, smtpServerPort, emailSenderAccount, emailSenderPassword);
-
-  // For library version 1.2.0 and later which STARTTLS protocol was supported,the STARTTLS will be
-  // enabled automatically when port 587 was used, or enable it manually using setSTARTTLS function.
-  //smtpData.setSTARTTLS(true);
 
   // Set the sender name and Email
   smtpData.setSender("ESP32", emailSenderAccount);
@@ -117,18 +137,17 @@ void sendEmail(int LED, int msg) {
   // Set the message with HTML format
   if (msg == 0) {
     smtpData.setMessage("<div style=\"color:#2f4468;\"><h1>Supplies Request</h1><p>- WE NEED MARKERS (sent from the POD)</p></div>", true);
-    // Set the email message in text format (raw)
-    //smtpData.setMessage("WE NEED MARKERS! -sent from the POD", false);
   }
   if (msg == 1) {
     smtpData.setMessage("<div style=\"color:#2f4468;\"><h1>Temperature Request</h1><p>- ITS HOT IN HERE (sent from the POD)</p></div>", true);
-    // Set the email message in text format (raw)
-    //smtpData.setMessage("WE NEED MARKERS! -sent from the POD", false);
+  }
+
+  if (msg == 2) {
+    smtpData.setMessage("<div style=\"color:#2f4468;\"><h1>Temperature Request</h1><p>- ITS COLD IN HERE (sent from the POD)</p></div>", true);
   }
 
   // Add recipients, you can add more than one recipient
   smtpData.addRecipient(emailRecipient);
-  //smtpData.addRecipient("YOUR_OTHER_RECIPIENT_EMAIL_ADDRESS@EXAMPLE.com");
 
   smtpData.setSendCallback(sendCallback);
 
@@ -139,6 +158,16 @@ void sendEmail(int LED, int msg) {
   //Clear all data from Email object to free memory
   smtpData.empty();
   digitalWrite(LED, HIGH);
+}
+
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
 }
 
 // Callback function to get the Email sending status
